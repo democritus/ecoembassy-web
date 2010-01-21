b5bbe1c2c311d9f3a81652f281aebaf63fc93f54
Brian Warren <brian.scott.warren@gmail.com>
2010-01-20 23:00:50 -0600

    added missing contact.html file

---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+set :application, "ecoembassy.org"
+set :repository,  "git@github.com:democritus/ecoembassy-web.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+# This should be the default, so left commented
+#set :deploy_to, "/u/apps/#{application}"
+
+server "luna.qlogicinc.com", :app, :web, :db, :primary => true # alternative syntax to "role"
+#role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+#role :app, "your app-server here"                          # This may be the same as your `Web` server
+#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+# What does this do? Creates config file /config/mongrel_cluster.yml??
+set :mongrel_port, "8020"
+set :mongrel_nodes, "5"
+
+# Deploying with git seems to require: default_run_options[:pty] = true
+# http://groups.google.com/group/capistrano/browse_thread/thread/13b029f75b61c09d
+default_run_options[:pty] = true
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
